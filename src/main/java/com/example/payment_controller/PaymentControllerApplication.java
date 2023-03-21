8e8fea916d97648d1ec0ed31933d1409b081a87a
DmitriyKholin <105742934+DmitriyKholin@users.noreply.github.com>
2023-03-21 20:32:16 +0300

    Commit

--- src/main/java/com/example/payment_controller/PaymentControllerApplication.java
+++ src/main/java/com/example/payment_controller/PaymentControllerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PaymentControllerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PaymentControllerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentControllerApplication.class, args);
+    }
 
 }
--- src/main/java/com/example/payment_controller/PaymentRequest.java
+++ src/main/java/com/example/payment_controller/PaymentRequest.java
@@ -1,2 +1,31 @@
-package com.example.payment_controller;public class PaymentRequest {
+package com.example.payment_controller;
+
+public class PaymentRequest {
+    private  int userId;
+    private  String itemId;
+    private  double discount;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(String itemId) {
+        this.itemId = itemId;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
 }
