c62f9ba726360073b58c7c88fe824acb5eb1dab7
DmitriyKholin <105742934+DmitriyKholin@users.noreply.github.com>
2023-03-21 20:23:30 +0300

    Initial commit

---
+++ src/main/java/com/example/payment_controller/PaymentController.java
@@ -0,0 +1,2 @@
+package com.example.payment_controller;public class PaymentController {
+}
---
+++ src/main/java/com/example/payment_controller/PaymentControllerApplication.java
@@ -0,0 +1,13 @@
+package com.example.payment_controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentControllerApplication.class, args);
+	}
+
+}
